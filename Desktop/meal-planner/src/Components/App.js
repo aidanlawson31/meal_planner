a03a9994dd0a3ace267a212ea2ac0d833a3071b7
Hayden <hayden.fuller@gmail.com>
2019-11-19 12:23:39 +1100

    added states to app.js and handle functions

---
+++ Desktop/meal-planner/src/Components/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Form from './Form'
+import ViewMealModel from './ViewMealModel'
+import './App.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+
+class App extends Component {
+  state = {
+    meals: [],
+    mealsCart: []
+    }
+
+    // Handling new meals
+    handleNewMeal = (meal) => {
+      this.setState({
+        meals: [...this.state.meals, meal]
+      })
+    };
+
+    // Retrieves all blogs at start of the app
+    handleBlogs = (blogs) => {
+      this.setState({blogs})
+    }
+
+  render() { 
+    const {meals} = this.state
+    const {mealsCart} = this.state
+    return ( 
+      <div className='layout'>
+         
+     
+
+      </div>
+    )
+  };
+}
+ 
+export default App;
